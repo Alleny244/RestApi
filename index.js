52714c8a7de03b371e486608e13f8279de270e37
Alleny244 <56961826+Alleny244@users.noreply.github.com>
2021-01-19 22:57:09 +0530

    modified

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
 const express=require('express');
-if (process.env.NODE_ENV !== "production") {
-    require("dotenv").config();
-  }
 
 const app=express();
 const mongoose=require('mongoose');
 
 const PORT=3005 || process.env.PORT;
 
-mongoose.connect(process.env.url,{ useUnifiedTopology: true },{ useNewUrlParser: true } );
+mongoose.connect("mongodb+srv://allen:789@first.6gdnr.mongodb.net/people?retryWrites=true&w=majority",{ useUnifiedTopology: true },{ useNewUrlParser: true } );
 const db=mongoose.connection;
 db.on("error",error=>console.error(error));
 db.once("open",open=>console.log("Database Connected"));
